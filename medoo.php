520650a6cb316026149273ee118075e221c060e1
andriusGecius <andrius.gecius@gmail.com>
2020-11-09 12:50:50 +0100

    Bug fix

--- medoo.php
+++ medoo.php
@@ -148,7 +148,7 @@ class Medoo
 					$dsn,
 					$this->username,
 					$this->password,
-					$this->option
+					(empty($this->option) === false ? $this->option : '')
 				);
 
 				foreach ($commands as $value)
